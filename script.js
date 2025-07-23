04c07495e7d391b8218b796e1325575cc02fd4f9
AMARDEEP KUMAR <126953722+Ashu-Cipher@users.noreply.github.com>
2025-07-23 19:20:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+
+const text = "Ethical Hacker | Cybersecurity Enthusiast";
+let index = 0;
+let forward = true;
+const speed = 100;
+const textElement = document.getElementById("typewriter-text");
+
+function typeEffect() {
+  if (forward) {
+    if (index < text.length) {
+      textElement.textContent += text.charAt(index);
+      index++;
+      setTimeout(typeEffect, speed);
+    } else {
+      forward = false;
+      setTimeout(typeEffect, 1500);
+    }
+  } else {
+    if (index > 0) {
+      textElement.textContent = text.substring(0, index - 1);
+      index--;
+      setTimeout(typeEffect, speed / 2);
+    } else {
+      forward = true;
+      setTimeout(typeEffect, 800);
+    }
+  }
+}
+
+typeEffect();
