7a629ce2f4c969c8a3c6156ad1d0d7ad2cc1e6ad
Ashu-Cipher <amardeepkumarbha@gmail.com>
2026-06-19 20:45:28 +0530

    debuggesd path error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Cursor from "@/components/layout/Cursor";
+import Navbar from "@/components/layout/Navbar";
+import Footer from "@/components/layout/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
